2409beacd8090e89ca0027cac88036e6a1588abb
Navigator <seanavigator2@gmail.com>
2022-09-26 12:56:23 +0300

    lesson 3

--- First_Django_Application/students/models.py
+++ First_Django_Application/students/models.py
@@ -3,6 +3,11 @@ from datetime import date
 from django.core.validators import MinLengthValidator
 from django.db import models
 
+from .validators import valid_email_domains, ValidEmailDomain
+from faker import Faker
+
+VALID_DOMAIN_LIST = ('@mail.com', '@gmail.com', '@yahoo.com')
+
 
 class Student(models.Model):
     first_name = models.CharField(
@@ -20,10 +25,27 @@ class Student(models.Model):
     )
     birthday = models.DateField(default=date.today, null=True, blank=True)
 
+    email = models.EmailField(validators=[ValidEmailDomain(*VALID_DOMAIN_LIST)])
+
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.pk}{self.first_name} {self.last_name}'
 
     class Meta:
-        db_table = 'student_table'
+        db_table = 'students'
 
+    @classmethod
+    def generate_fake_data(cls, cnt):
+        f = Faker()
+
+        for _ in range(cnt):
+            first_name = f.first_name()
+            last_name = f.last_name()
+            email = f'{first_name}.{last_name}{f.random.choice(VALID_DOMAIN_LIST)}'
+            birthday = f.date()
+            st = cls(first_name=first_name, last_name=last_name, birthday=birthday, email=email)
+            try:
+                st.full_clean()
+                st.save()
+            except:
+                print(f'Incorrect data {first_name}, {last_name}, {birthday}, {email}')
 
--- First_Django_Application/students/views.py
+++ First_Django_Application/students/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render # noqa
+from django.http import HttpResponse
+from django.shortcuts import render  # noqa
 
-# Create your views here.
+from students.models import Student
+
+
+def index(request):
+    students = Student.objects.all()
+    s = '<table>'
+    for student in students:
+        s += f'<tr><td>{student.first_name}</td><td>{student.last_name} </td><td>{student.email}</td></tr>'
+    s += '</table>'
+
+    response = HttpResponse(s)
+    return response
