8b6434732de634fa9838b7a25cb7fd1951fce8d3
Navigator <seanavigator2@gmail.com>
2022-10-05 12:44:36 +0300

    lesson 6

--- First_Django_Application/students/models.py
+++ First_Django_Application/students/models.py
@@ -1,6 +1,9 @@
 from datetime import date
 
+
 from core.validators import ValidEmailDomain, validate_unique_email, validate_unique_phone
+
+from dateutil.relativedelta import relativedelta
 
 from django.core.validators import MinLengthValidator
 from django.db import models
@@ -33,6 +36,9 @@ class Student(models.Model):
 
     def __str__(self):
         return f'{self.pk}{self.first_name} {self.last_name}'
+
+    def get_age(self):
+        return relativedelta(date.today(), self.birthday).years
 
     class Meta:
         db_table = 'students'
